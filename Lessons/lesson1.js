8ca4405ed08cba39b6fbc8f616c8e187da0206a7
YADU DEV R <104296438+ryadudev@users.noreply.github.com>
2025-09-02 13:00:38 +0530

    1
    1

--- Lessons/lesson1.js
+++ Lessons/lesson1.js
@@ -1,18 +1,15 @@
 //1. Hello Worl
-
     console.log("Hello world!!!!!")
-
 
 //Variables.
 /*
 Variable can be defined as var or let
 */
-
 var firstName = "Yadu"
 let lastName = "R"
 console.log(firstName)
 console.log(lastName)
-
+//OR we can decalre as below
 var age, dOB, sex
 age = "5"
 sex = "Male"
@@ -25,11 +22,28 @@ console.log(age)
 //Constants.
  
 const occupation = "Engineer"
+console.log(occupation)
 /*
 occupation = "Sales"
 
 in the above line we are trying to override the constant variable
 occupaton with a new value. This will throw an error.
+
+We cannot declare a constant as: const age, sex, dOB
 */
 
-console.log(occupation)
+//Datatypes.
+/*
+We have 5 primitive datatypes.
+*/
+
+var fName = "Yadu" //String
+var age = 32 //Number
+var married  = true //Boolean: it can be 'false' as well.
+var visitedUS = null //Null
+var noOfCars = undefined // Undefined
+
+
+
+
+
---
+++ Lessons/lesson2.js
@@ -0,0 +1,22 @@
+//Concatination and Interpolation
+
+var messageToPrint = "The price for your cup is 50 dollars"
+console.log(messageToPrint)
+/*
+here in the above code variables are cup and price which will change.
+*/
+
+var product = "table"
+var price = 44440
+//Replace the variable names.
+var messageToPrint2 = "The price of your " + product + " is " + price + " dollars: Printed using CONCATINATION"  
+console.log(messageToPrint2)
+
+//Interpolation..
+var messageToPrint3 = `The price of your item ${product} is ${price} dollars : printed using INTERPOLATION`
+console.log(messageToPrint3)
+
+/*
+
+*/
+

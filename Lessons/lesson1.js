2d8345edfedfaed036a814f8aec5eed94be25901
YADU DEV R <104296438+ryadudev@users.noreply.github.com>
2025-09-02 12:38:12 +0530

    Initial commit

---
+++ Lessons/lesson1.js
@@ -0,0 +1,33 @@
+//1. Hello Worl
+
+    console.log("Hello world!!!!!")
+
+
+//Variables.
+/*
+Variable can be defined as var or let
+*/
+
+var firstName = "Yadu"
+let lastName = "R"
+console.log(firstName)
+console.log(lastName)
+
+var age, dOB, sex
+age = "5"
+sex = "Male"
+console.log(age)
+
+//Overriding the variable age
+age = "6"
+console.log(age)
+
+//Constants.
+ 
+const occupation = "Engineer"
+/*
+occupation = "Sales"
+
+in the above line we are trying to override the constant variable
+occupaton with a new value. This will throw an error.
+*/
